2f9d7377249fb894fd2f46bf10e9c8c02e180f2a
sim.29 <simran.tuteja29@gmail.com>
2021-07-21 23:17:19 +0530

    basic UI with hover effect

--- src/Row.js
+++ src/Row.js
@@ -1,23 +1,37 @@
 import React from "react";
 import axios from "./axios";
+import "./Row.css";
 import { useState, useEffect } from "react";
 
 function Row(props) {
   const [movies, setmovies] = useState([]);
+  const imgPath = "https://image.tmdb.org/t/p/original";
 
   useEffect(() => {
     async function fetchData() {
       const req = await axios.get(props.fetchURL);
-      console.log(req);
+      console.table(req.data.results);
+      console.log(req.data.results);
+      setmovies(req.data.results);
+
       return req;
     }
     fetchData();
-  }, []);
+  }, [props.fetchURL]);
   return (
     <div>
       {/* title */}
       <h1>{props.title}</h1>
       {/* poster */}
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="poster"
+            src={`${imgPath}${movie.poster_path}`}
+            alt={movie.title}
+          />
+        ))}
+      </div>
     </div>
   );
 }
